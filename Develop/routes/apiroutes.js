3ac1d2232c4d71d5b0b0334ca6de345c3b1e145d
Melina Boedecker <melinamboedecker@gmail.com>
2021-06-13 00:00:08 -0700

    moved db folder into public folder and adjusted paths

--- Develop/routes/apiroutes.js
+++ Develop/routes/apiroutes.js
@@ -7,7 +7,7 @@ const uniqid = require('uniqid');
 module.exports = (app) => {
     // => API GET requests
     app.get('/api/notes', (req, res) => {
-        fs.readFile("./Develop/db/db.json/", (err,data) => {
+        fs.readFile("./Develop/public/db/db.json/", "utf8", (err,data) => {
             if (err) {
                 throw err;
             }
@@ -19,19 +19,19 @@ module.exports = (app) => {
     app.post('/api/notes', (req, res) => {
         const note = req.body;
         note.id = uniqid();
-        const database = JSON.parse(fs.readFileSync("./Develop/db/db.json/"));
+        const database = JSON.parse(fs.readFileSync("./Develop/public/db/db.json/"));
         database.push(note);
-        fs.writeFileSync('./Develop/db/db.json', JSON.stringify(database));
+        fs.writeFileSync('./Develop/public/db/db.json', JSON.stringify(database));
         res.json ("your note has been saved")
     });
 
     app.delete('/api/notes/:id', (req, res) => {
         const deleId = req.params.id;
-        const dataToDele = JSON.parse(fs.readFileSync("./Develop/db/db.json/"));
+        const dataToDele = JSON.parse(fs.readFileSync("./Develop/public/db/db.json/"));
         let filtered = dataToDele.filter(function (el) {
             return el.id != deleId;
         });
-        fs.writeFileSync('./Develop/db/db.json', JSON.stringify(filtered));
+        fs.writeFileSync('./Develop/public/db/db.json', JSON.stringify(filtered));
         res.json ("your note has been deleted")
     })
 
