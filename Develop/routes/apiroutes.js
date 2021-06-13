8a70a4455146c182d0efa934a141c7598ee4e76f
Melina Boedecker <melinamboedecker@gmail.com>
2021-06-13 01:25:58 -0700

    added constant for path to json file

--- Develop/routes/apiroutes.js
+++ Develop/routes/apiroutes.js
@@ -4,12 +4,17 @@ const fs = require("fs");
 const path = require('path');
 const uniqid = require('uniqid');
 
+const dbdatapath  = path.join(__dirname, '../public/db/db.json');
+console.log('CONST++++++++++++CONST')
+console.log(dbdatapath);
+
 module.exports = (app) => {
     // => API GET requests
     app.get('/api/notes', (req, res) => {
         console.log(__dirname);
         console.log('++++++++++++++++s')
-        fs.readFile("./Develop/public/db/db.json", "utf8", (err,data) => {
+        console.log(dbdatapath);
+        fs.readFile(dbdatapath, "utf8", (err,data) => {
             if (err) {
                 throw err;
             }
@@ -21,19 +26,19 @@ module.exports = (app) => {
     app.post('/api/notes', (req, res) => {
         const note = req.body;
         note.id = uniqid();
-        const database = JSON.parse(fs.readFileSync("./Develop/public/db/db.json/"));
+        const database = JSON.parse(fs.readFileSync(dbdatapath));
         database.push(note);
-        fs.writeFileSync('./Develop/public/db/db.json', JSON.stringify(database));
+        fs.writeFileSync(dbdatapath, JSON.stringify(database));
         res.json ("your note has been saved")
     });
 
     app.delete('/api/notes/:id', (req, res) => {
         const deleId = req.params.id;
-        const dataToDele = JSON.parse(fs.readFileSync("./Develop/public/db/db.json/"));
+        const dataToDele = JSON.parse(fs.readFileSync(dbdatapath));
         let filtered = dataToDele.filter(function (el) {
             return el.id != deleId;
         });
-        fs.writeFileSync('./Develop/public/db/db.json', JSON.stringify(filtered));
+        fs.writeFileSync(dbdatapath, JSON.stringify(filtered));
         res.json ("your note has been deleted")
     })
 
