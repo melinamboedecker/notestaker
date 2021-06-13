45c3d37d114249a0475c48356874c99533bfa68e
Melina Boedecker <melinamboedecker@gmail.com>
2021-06-12 23:47:11 -0700

    fixed titles in server.js lines 13 and 14

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ app.use(express.json());
 app.use(express.static(__dirname + '/Develop/public'));
 app.use(express.static(__dirname + '/Develop/db'));
 
-require('./Develop/routes/apiRoutes')(app);
-require('./Develop/routes/htmlRoutes')(app);
+require('./Develop/routes/apiroutes')(app);
+require('./Develop/routes/htmlroutes')(app);
 
 app.listen(PORT, () => {
     console.log(`App listening on PORT: ${PORT}`);
